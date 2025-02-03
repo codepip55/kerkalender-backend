afb5790afab8e8fa19fb237690f95de4f785a4c7
Pip <92968130+codepip55@users.noreply.github.com>
2025-02-03 09:15:22 +0100

    Make models and start service controller

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,10 @@ class User extends Authenticatable implements MustVerifyEmail
             'password' => 'hashed',
         ];
     }
+    public function teams() {
+        return $this->belongsToMany(Team::class)->withPivot('status');
+    }
+    public function services_managed() {
+        return $this->hasMany(Service::class, 'service_manager_id');
+    }
 }
