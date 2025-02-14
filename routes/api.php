15fc66b647cf5eeec90c4e9548a3f7ac7e2223cc
Pip <92968130+codepip55@users.noreply.github.com>
2025-02-14 16:04:45 +0100

    Internal Auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InternalAuthController;
+
+Route::prefix('api')->group(function () {
+    // Internal Auth
+    Route::middleware('auth:api')->get('/auth/kerkalender', [InternalAuthController::class, 'getUserFromLogin']);
+    Route::post('/auth/silent', [InternalAuthController::class, 'refresh']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
